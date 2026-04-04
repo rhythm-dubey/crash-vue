2ac8f7d5c48b009a0b2289707f5b76794dd029cc
Rhythm Dubey <rhythm@itradicals.com>
2026-04-04 12:39:33 +0530

    Installed bootstrap and implemented modal

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -3,18 +3,30 @@
     {{ count }}
   </h1>
   <br />
-  <button @click='increment'>+</button>
-  <button @click='decrement'>-</button>
+  <button @click='increment'
+    class='btn btn-primary'
+    >
+    +</button>
+  <button @click='decrement'
+    class='btn btn-danger'
+    >-</button>
+
+  <ModalComponent />
 </template>
 
 
 
 <script>
+import ModalComponent from './ModalComponent.vue';
+
 export default {
   data() {
     return {
       count: 0
     }
+  },
+  components: {
+    ModalComponent
   },
   methods: {
     increment() {
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- Button -->
+    <button class="btn btn-primary" @click="openModal('Hello', 'This is dynamic content')">
+      Open Modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade show" tabindex="-1" style="display: block;" v-if="isOpen">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          
+          <div class="modal-header">
+            <h5 class="modal-title">{{ modalData.title }}</h5>
+            <button class="btn-close" @click="closeModal"></button>
+          </div>
+
+          <div class="modal-body">
+            <p>{{ modalData.body }}</p>
+          </div>
+
+          <div class="modal-footer">
+            <button class="btn btn-secondary" @click="closeModal">Close</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Backdrop -->
+    <div class="modal-backdrop fade show" v-if="isOpen"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+const modalData = ref({
+  title: '',
+  body: ''
+})
+
+const openModal = (title, body) => {
+  modalData.value = { title, body }
+  isOpen.value = true
+}
+
+const closeModal = () => {
+  isOpen.value = false
+}
+</script>
