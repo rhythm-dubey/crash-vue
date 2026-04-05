8e25733aa019cb9152f7f253ac4ce25857fc1df1
Rhythm Dubey <rhythm@itradicals.com>
2026-04-05 09:58:08 +0530

    Updated indentation

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <button class="btn btn-primary" @click="openModal">
-      Open Modal
-    </button>
+<div>
+	<button class="btn btn-primary" @click="openModal">
+		Open Modal
+	</button>
 
-    <div class="modal fade" id="myModal" ref="modalRef" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          
-          <div class="modal-header">
-            <h5 class="modal-title">Hello</h5>
-            <button class="btn-close" data-bs-dismiss="modal"></button>
-          </div>
+	<div class="modal fade" id="myModal" ref="modalRef" tabindex="-1">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		
+		<div class="modal-header">
+			<h5 class="modal-title">Hello</h5>
+			<button class="btn-close" data-bs-dismiss="modal"></button>
+		</div>
 
-          <div class="modal-body">
-            <p>This is modal</p>
-          </div>
+		<div class="modal-body">
+			<p>This is modal</p>
+		</div>
 
-        </div>
-      </div>
-    </div>
-  </div>
+		</div>
+	</div>
+	</div>
+</div>
 </template>
 
 <script setup>
@@ -31,14 +31,14 @@ const modalRef = ref(null)
 let modalInstance = null
 
 onMounted(() => {
-  if (modalRef.value) {
-    modalInstance = new Modal(modalRef.value)
-  }
+if (modalRef.value) {
+	modalInstance = new Modal(modalRef.value)
+}
 })
 
 const openModal = () => {
-  if (modalInstance) {
-    modalInstance.show()
-  }
+if (modalInstance) {
+	modalInstance.show()
+}
 }
 </script>
